c9a92a06b868655b603aa31d2237d0222f2b18ca
mrparkersson <amoparkerprince78@gmail.com>
2022-07-03 08:18:57 +0000

    Import card list styles into card list component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,16 @@
 import { Component } from 'react';
-
+import Card from '../card/card.component';
+import './card-list.styles.css';
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
-      <>
+      <div className="card-list">
         {monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
+          const { name, email, id } = monster;
+          return <Card name={name} email={email} id={id} />;
         })}
-      </>
+      </div>
     );
   }
 }
