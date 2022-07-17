e65da51f63e6955a6af58800bb6425f6ebfc93cb
mrparkersson <amoparkerprince78@gmail.com>
2022-07-17 12:57:13 +0000

    Add return statement

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ const CardList = ({ monsters }) => {
     <div className="card-list">
       {monsters.map((monster) => {
         const { name, email, id } = monster;
-        return <Card name={name} email={email} id={id} />;
+        return <Card name={name} email={email} id={id} key={name} />;
       })}
     </div>
   );
